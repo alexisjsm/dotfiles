d13020b7f55efd73f32efe6e9bb8d674ab7ad857
Alexis Jesus <alexis.sierra@councilbox.com>
2022-10-08 00:01:22 +0200

    update install

---
+++ start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "Hola"
+echo "primero actualizaremos el sistema"
+sudo apt update && sudo apt upgrade -y
+echo "instalamos zsh"
+sudo apt install zsh vscode fonts-firacode -y
+
+echo "instalamos volta"
+curl https://get.volta.sh | bash
+echo "instalando node y yarn"
+~/.volta/bin/volta install node@lts yarn 
+
+echo "instalando OH-MY-ZSH"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "instalando extensiones..."
+
+echo "clonando zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "clonando zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "clonamos powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "nos movemos a .config"
+cd ~/.config/
+
+echo "clonamos dotfiles"
+git clone https://github.com/alexisjsm/dotfiles.git
+
+echo "enlanzamos"
+ln -s ~/.config/dotfiles/.zshrc  ~/.zshrc
+ln -s ~/.config/dotfiles/.zshrc  ~/.vimrc
