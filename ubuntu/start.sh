f8b0e8d780fd5a87d6bc0060a6e1fc9e83a85feb
Alexis Jesús <cv.alexisjesus@gmail.com>
2023-06-20 21:30:05 +0200

    fixed ubuntu

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -4,6 +4,9 @@ echo "primero actualizaremos el sistema"
 sudo sudo apt update -y && sudo apt upgrade -y
 sleep 5
 
+echo "instalamos zsh"
+sudo apt get zsh -y
+sleep 3
 echo "agregando zsh como interprete por defecto"
 chsh -s $(which zsh)
 sleep 3
@@ -19,21 +22,17 @@ echo "instalando OH-MY-ZSH"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sleep 3
 
-echo "haciendo zsh por defecto"
-chsh -s $(which zsh)
-
 echo "instalando extensiones..."
-
+sleep 5
 echo "clonando zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+sleep 5
 echo "clonando zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+sleep 5
 echo "clonamos powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-sleep 3
+sleep 5
 
 echo "nos movemos a .config"
 cd ~/.config/
@@ -47,6 +46,9 @@ sleep 3
 
 echo "enlanzamos"
 ln -s ~/.config/dotfiles/.zshrc  ~/.zshrc
+sleep 3
 ln -s ~/.config/dotfiles/.vimrc  ~/.vimrc
+sleep 3
 ln -s ~/.config/dotfiles/.p10k.zsh ~/.p10k.zsh
+sleep 3
 echo "fin"
