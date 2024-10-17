8f8c853724247b4e4dc7a81333090efca51dc442
Alexis Jesús <cv.alexisjesus@gmail.com>
2024-10-17 23:02:26 +0200

    refactor installations

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 echo "Hola"
 echo "primero actualizaremos el sistema"
-sudo sudo apt update -y && sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y
 sleep 5
 
 echo "instalamos zsh"
 sudo apt install zsh -y
 sleep 10
 echo "agregando zsh como interprete por defecto"
-chsh -s $(which zsh)
+chsh -s $(which zsh) $USER
 sleep 10
 
 echo "instalamos volta"
 curl https://get.volta.sh | bash
 
 echo "instalando node y yarn"
-~/.volta/bin/volta install node@lts yarn 
-sleep 3
+~/.volta/bin/volta install node@lts
+
+sleep 5
 
 echo "instalando OH-MY-ZSH"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sleep 3
+sleep 5
 
 echo "instalando extensiones..."
 sleep 5
@@ -34,21 +35,27 @@ echo "clonamos powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sleep 5
 
+echo "comprobando carpeta .config"
+if [ ! -d ~/.config ]; then
+		echo "creando .config"
+		mkdir ~/.config
+fi
+
 echo "nos movemos a .config"
 cd ~/.config/
 
 echo "clonamos dotfiles"
 git clone https://github.com/alexisjsm/dotfiles.git
 
-echo "borrado por defecto"
-rm ~/.zshrc
-sleep 3
+sleep 5
 
-echo "enlanzamos"
-ln -s ~/.config/dotfiles/.zshrc  ~/.zshrc
-sleep 3
-ln -s ~/.config/dotfiles/.vimrc  ~/.vimrc
-sleep 3
-ln -s ~/.config/dotfiles/.p10k.zsh ~/.p10k.zsh
-sleep 3
+echo "enlazando .zshrc"
+ln -sf ~/.config/dotfiles/.zshrc  ~/.zshrc
+sleep 5
+echo "enlazando .vimrc"
+ln -sf ~/.config/dotfiles/.vimrc  ~/.vimrc
+sleep 5
+echo "enlazando p10k"
+ln -sf ~/.config/dotfiles/.p10k.zsh ~/.p10k.zsh
+sleep 5
 echo "fin"
